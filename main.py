185a79e5e9894bae180cf5a6b6e4209d5543f73c
Rafael Gomes <o.rafaelgomes11@gmail.com>
2026-06-25 20:16:21 -0300

    Adicionar menus de cadastro e fluxo de registro de hardware

--- main.py
+++ main.py
@@ -5,8 +5,35 @@ while True:
     chosen_option = menu.primary_ui()
     if chosen_option == 1:
         print(f'\nFunção indisponível!\n')
+        
     elif chosen_option == 2: 
-        print(f'\nFunção indisponível!\n')
+        chosen_option = menu.register_ui()
+        if chosen_option == 1:
+            chosen_option = menu.hardware_register_ui()
+            if chosen_option == 1:
+                print(f'\nFunção indisponível!\n')
+            elif chosen_option == 2:
+                print(f'\nFunção indisponível!\n')
+            elif chosen_option == 3:
+                print(f'\nFunção indisponível!\n')
+            elif chosen_option == 4:
+                print(f'\nFunção indisponível!\n')
+            elif chosen_option == 5:
+                print(f'\nFunção indisponível!\n')
+            elif chosen_option == 6:
+                print(f'\nFunção indisponível!\n')
+            elif chosen_option == 7:
+                print(f'\nFunção indisponível!\n')
+            elif chosen_option == 8:
+                print(f'\nFunção indisponível!\n')
+            elif chosen_option == 9:
+                chosen_option = menu.register_ui()
+                
+        elif chosen_option == 2:
+            print(f'\nFunção indisponível!\n')
+        elif chosen_option == 3: 
+            chosen_option = menu.primary_ui()
+        
     elif chosen_option == 3:  
         print(f'\nFunção indisponível!\n')
     elif chosen_option == 4: 
--- menu.py
+++ menu.py
@@ -11,3 +11,28 @@ def primary_ui ():
         limit_option = 4
         chosen_option = utils.test(option, limit_option)
         return chosen_option
+    
+def register_ui ():
+    while True:
+        print('---------- MENU DE CADASTRO ----------')
+        print('1. Cadastrar componentes.')
+        print('2. Cadastrar computador.')
+        print('3. Retornar ao meu inicial.')
+        limit_option = 3
+        chosen_option = utils.test(option, limit_option)
+        return chosen_option
+
+def hardware_register_ui ():
+    print('---------- MENU DE CADASTRO DE COMPONENTES ----------')
+    print('1. Cooler.')
+    print('2. Gabinete.')
+    print('3. Fonte.')
+    print('4. HDD ou SSD.')
+    print('5. Memória RAM.')
+    print('6. Placa mãe.')
+    print('7. Placa de vídeo.')
+    print('8. Processador.')
+    print('9. Retornar ao menu inicial.')
+    limit_option = 3
+    chosen_option = utils.test(option, limit_option)
+    return chosen_option
